8ac0749059fc9551fcf3daa1d49e122abded4eeb
shreya chakraborty <111153292+shreya020703@users.noreply.github.com>
2024-06-07 21:13:47 +0530

    final commit

---
+++ Question1.m
@@ -0,0 +1,12 @@
+// 1. Display an image to illustrate change in image quality with decreasing gray levels-128, 64, 32, 16 and 8. 
+
+
+img = imread('cameraman.tif');
+figure, subplot(2,3,1), imshow(img), title('Original Image')
+
+
+gray_levels = [128, 64, 32, 16, 8];
+for i = 1:length(gray_levels)
+    reduced_img = uint8(floor(double(img) / (256 / gray_levels(i))) * (256 / gray_levels(i)));
+    subplot(2,3,i+1), imshow(reduced_img), title(['Gray Levels: ', num2str(gray_levels(i))])
+end
